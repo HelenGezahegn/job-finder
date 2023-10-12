e7e27dee2588b3259b14760821460d1709b71999
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-11 19:33:29 -0600

    feat: add header section of the welcome screen - Add welcome messsage - Add text input + search button  where user would search for the job - Add buttons under input field where they can filter between job types (e.g. full-time)

--- components/home/welcome/welcome.style.js
+++ components/home/welcome/welcome.style.js
@@ -4,25 +4,25 @@ import { COLORS, FONT, SIZES } from "../../../constants";
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
+    width: "100%"
   },
   userName: {
     fontFamily: FONT.regular,
     fontSize: SIZES.large,
-    color: COLORS.secondary,
+    color: COLORS.secondary
   },
   welcomeMessage: {
     fontFamily: FONT.bold,
     fontSize: SIZES.xLarge,
     color: COLORS.primary,
-    marginTop: 2,
+    marginTop: 2
   },
   searchContainer: {
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "row",
     marginTop: SIZES.large,
-    height: 50,
+    height: 50
   },
   searchWrapper: {
     flex: 1,
@@ -31,13 +31,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: SIZES.medium,
-    height: "100%",
+    height: "100%"
   },
   searchInput: {
     fontFamily: FONT.regular,
     width: "100%",
     height: "100%",
-    paddingHorizontal: SIZES.medium,
+    paddingHorizontal: SIZES.medium
   },
   searchBtn: {
     width: 50,
@@ -45,28 +45,28 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.tertiary,
     borderRadius: SIZES.medium,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "center"
   },
   searchBtnImage: {
     width: "50%",
     height: "50%",
-    tintColor: COLORS.white,
+    tintColor: COLORS.white
   },
   tabsContainer: {
     width: "100%",
-    marginTop: SIZES.medium,
+    marginTop: SIZES.medium
   },
   tab: (activeJobType, item) => ({
     paddingVertical: SIZES.small / 2,
     paddingHorizontal: SIZES.small,
     borderRadius: SIZES.medium,
     borderWidth: 1,
-    borderColor: activeJobType === item ? COLORS.secondary : COLORS.gray2,
+    borderColor: activeJobType === item ? COLORS.secondary : COLORS.gray2
   }),
   tabText: (activeJobType, item) => ({
     fontFamily: FONT.medium,
-    color: activeJobType === item ? COLORS.secondary : COLORS.gray2,
-  }),
+    color: activeJobType === item ? COLORS.secondary : COLORS.gray2
+  })
 });
 
 export default styles;
