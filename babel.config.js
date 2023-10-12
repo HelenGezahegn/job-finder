be2ecf4927aef3c50b18ca3cf75faa8d81018448
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-11 20:34:25 -0600

    fix: allow .env variables

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,14 @@ module.exports = function (api) {
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
-      require.resolve("expo-router/babel"),
-    ],
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env"
+        }
+      ],
+      require.resolve("expo-router/babel")
+    ]
   };
 };
