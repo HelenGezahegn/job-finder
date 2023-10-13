6a5887bfa8f8ea1724cbcf90f2697d74d7498602
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-12 22:30:14 -0600

    fix: wrong class name for the button text in job details footer

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = ({ url }) => {
         style={styles.applyBtn}
         onPress={() => Linking.openURL(url)}
       >
-        <Text style={styles.applyBtn}>Apply for job</Text>
+        <Text style={styles.applyBtnText}>Apply for job</Text>
       </TouchableOpacity>
     </View>
   );
